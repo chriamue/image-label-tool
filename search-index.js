5d0676753e649e5a9499045e31c6d5cfd889e0e3
chriamue <chriamue@users.noreply.github.com>
2022-12-17 14:19:48 +0000

    deploy: 3d4a01c289429458a5fe43c33971670e5f7a2110

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"image_label_tool":{"doc":"","t":[5],"n":["add"],"q":["image_label_tool"],"d":[""],"i":[0],"f":[[[1,1],1]],"p":[[15,"usize"]]}\
+"image_label_tool":{"doc":"","t":[5],"n":["init_label_tool"],"q":["image_label_tool"],"d":[""],"i":[0],"f":[[1]],"p":[[3,"Element"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
