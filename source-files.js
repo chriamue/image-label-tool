5b8ac8ffcd1fc47b77f2b210f9d4ea159964f375
chriamue <chriamue@users.noreply.github.com>
2023-04-02 16:54:05 +0000

    deploy: 15a967a38279740914da2387a66f1aa7ccd8c3d1

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"image_label_tool":["",[["components",[["status_bar",[],["mod.rs"]]],["images_list.rs","mod.rs"]],["stores",[],["image_store.rs","mod.rs"]]],["annotated_image.rs","app.rs","bbox.rs","download.rs","editor.rs","header.rs","label_tool.rs","labels.rs","lib.rs","upload_annotations.rs","upload_image.rs","use_canvas_image.rs","utils.rs"]]\
+"image_label_tool":["",[["components",[["status_bar",[],["mod.rs"]]],["add_image_button.rs","header.rs","images_list.rs","mod.rs"]],["stores",[],["image_store.rs","mod.rs"]]],["annotated_image.rs","app.rs","bbox.rs","download.rs","editor.rs","label_tool.rs","labels.rs","lib.rs","upload_annotations.rs","upload_image.rs","use_canvas_image.rs","utils.rs"]]\
 }');
 createSourceSidebar();
